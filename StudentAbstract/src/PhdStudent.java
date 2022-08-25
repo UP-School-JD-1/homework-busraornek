0e11916275e2a8cce8ddfe874172952697a15be4
Büşra Örnek <107885877+busraornek@users.noreply.github.com>
2022-08-25 16:23:16 +0300

    Add files via upload

---
+++ StudentAbstract/src/PhdStudent.java
@@ -0,0 +1,61 @@
+import java.sql.Date;
+
+public class PhdStudent extends GraduateStudent{
+	
+	protected boolean qualifyingExamTaken;
+	
+    // public PhdStudent (int no, String name, int year ,Date dob, String major, String advisor, String thesis, boolean qualifyingExamTaken) {
+    	 //public PhdStudent (int no, String name, int year , String major, String advisor, String thesis, boolean qualifyingExamTaken) {
+	PhdStudent(int no, String name, int year, String major,String advisor, String thesis,boolean qualifyingExamTaken){
+        super(no,name,year,major,advisor,thesis);
+        this.qualifyingExamTaken=qualifyingExamTaken;
+    }
+		//super(no, name , year, dob, major, advisor, thesis);
+		//super(no, name , year, major, advisor, thesis);
+		
+		//this.qualifyingExamTaken = qualifyingExamTaken; 
+
+   
+     @Override
+ 	
+ 	public void study() {
+ 		   
+ 		   System.out.println("I am phd study.");
+ 	}
+     @Override
+ 	 public void register(){
+    	   
+ 			
+    	 System.out.println("Fill in the information below to register.");
+  	     System.out.println("Name: " +name  );
+  	     System.out.println("Your year of birth:" + year);
+  	     System.out.println("Your school number: " + no);
+  	    // System.out.println("Date of registration: " + dob);
+
+  	     
+ 		   
+    	  
+ 	 }
+     @Override
+ 	 public void writethesis() {
+ 			
+ 			System.out.println("What are you planning to write a thesis about? " +thesis);
+ 			
+ 		}
+     @Override
+ 	 public void meetWithAdvisor() {
+ 	    	
+ 	    	System.out.println("Who is your advisor?  " +advisor);
+ 	    }
+     public void writePaper() {
+    	 
+    	 if(qualifyingExamTaken == true) {
+	    	System.out.println("You passed the exam");
+    	 }
+    	 else {
+    		 System.out.println("Your proficiency test was not found!");
+    		 
+    	 }
+     }
+ 	 
+}
---
+++ StudentAbstract/src/hw2/RegistrationOffice.java
@@ -0,0 +1,27 @@
+package hw2;
+
+
+public class RegistrationOffice {
+
+	public RegistrationOffice(){
+	
+	}
+	
+	
+	Student getAStudent(Student student) {
+	String studentname = student.getName();
+	System.out.println("Student's name: " + studentname);
+	return student;
+   }
+	
+	public void registerStudent(Student student) {
+		
+		System.out.println("Student registration information: ");
+		System.out.println("Student's name: " +student.getName());
+		System.out.println("Student number: " + student.getNo());
+		System.out.println("Student's branch: " +student.getMajor());
+		System.out.println("Student's registration date: " +student.getDob());
+		
+		
+	}
+}
